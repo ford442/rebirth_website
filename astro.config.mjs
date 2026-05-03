427475243f4ca8cd834be0f435be2df42705409a
root <noahc42@gmail.com>
2026-05-04 00:26:29 +0200

    codespace

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ford442.github.io',
-  base: '/rebirth_website',
+  base: '/rb338',
   compressHTML: true,
   build: {
     assets: '_assets',
